10e897b6ddaf41f9080d252418b803a162fc37ad
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2024-08-17 21:54:42 +0900

    9-153

--- 09 project Management/src/components/NoProjectSelected.jsx
+++ 09 project Management/src/components/NoProjectSelected.jsx
@@ -1,6 +1,6 @@
 import noProjectImage from '../assets/no-projects.png'
 import Button from './Button';
-export default function NoProjectSelected(){
+export default function NoProjectSelected({onStartAddProject}){
     return (
       <div className="mt-24 text-center w-2/3">
         <img
@@ -13,7 +13,7 @@ export default function NoProjectSelected(){
         </h2>
         <p className='text-stone-400 mb-4'>Select a project or get started with a new one</p>
         <p className=''>
-            <Button>Create new project</Button>
+            <Button onClick={onStartAddProject}>Create new project</Button>
         </p>
       </div>
     );
