ab061e71a9bf7638863a1f52cb8b3c685064aa0e
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2024-08-18 16:14:40 +0900

    modal style

--- 09 project Management/src/App.jsx
+++ 09 project Management/src/App.jsx
@@ -7,16 +7,24 @@ function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
     projects: [],
-    //선택된 프로젝트가 있는지?? , 생성된 프로젝트 담은 배열
   });
 
-  //프로젝트 만들기 함수
   function handleStartAddProject(){
     setProjectsState(prevState=>{
       return{
         ...prevState,
         selectedProjectId:null,
-         // 이전 상태selectedProjectId를 null로 설정하여 새 프로젝트 추가 모드로 전환
+         
+      };
+    });
+  }
+
+  function handleCancelAddProject(){
+    setProjectsState(prevState=>{
+      return{
+        ...prevState,
+        selectedProjectId:undefined,
+         
       };
     });
   }
@@ -35,12 +43,13 @@ function App() {
       }
     })
   }
+
   console.log(projectsState);
   let content;
 
 
   if(projectsState.selectedProjectId ===null){
-    content = <NewProject onAdd={handleAddProject} />
+    content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
   }
   // 프로젝트를 하나도 생성하지 않았을떄, 초기화면으로 보여주기
  else if (projectsState.selectedProjectId === undefined){
