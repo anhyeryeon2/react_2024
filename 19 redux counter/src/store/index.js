872188b8eaf4aa84f42f246a69f84ecd35b1413b
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2024-08-22 23:22:13 +0900

    redux toolkit

--- 19 redux counter/src/store/index.js
+++ 19 redux counter/src/store/index.js
@@ -1,31 +1,32 @@
-//먼저 새스토어를 만든다.
 
+import {createSlice , configureStore} from '@reduxjs/toolkit';
 
-import {createStore} from 'redux';
+const initialState = {counter :0, showCounter :true};
 
-const counterReducer = (state={counter :0},action) =>{
-    if (action.type ==='increment'){
-        return{
-            counter:state.counter+1
+const counterSlice = createSlice({
+    name:'counter',
+    initialState : initialState,
+    reducers:{
+        increment(state){
+            state.counter++;
+        },
+        decrement(state){
+            state.counter--;
+        },
+        increase(state,action){
+            state.counter = state.counter+action.payload;
+        },
+        toggleCounter(state){
+            state.showCounter= !state.showCounter;
         }
     }
-    if (action.type ==='increase'){
-        return{
-            counter:state.counter+action.amount
-        }
-    }
-    if (action.type ==='decrement'){
-        return{
-            counter:state.counter-1
-        }
-    }
-    return state;
-}
-const store = createStore(counterReducer);
-
-//createStore에는 포인터가 있어야함
-//리듀서함수에서 매개변수로
+});
 
 
-//리액트 앱과 리덕스 스토어를 연결하고싶다
+const store = configureStore({
+    reducer: counterSlice.reducer
+});
+
+export const counterActions = counterSlice.actions;
+
 export default store;
