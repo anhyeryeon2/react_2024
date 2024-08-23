8d3625463da5423654751737b16f836668ad042a
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2024-08-23 21:45:08 +0900

    redux http state

--- 20 advanced redux/src/components/store/cart-slice.js
+++ 20 advanced redux/src/components/store/cart-slice.js
@@ -7,11 +7,17 @@ const cartSlice = createSlice({
     totalQuantity: 0,
   },
   reducers: {
+    replaceCart(state, action) {
+      // 장바구니의 총 수량과 아이템을 새로운 데이터로 교체
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
       if (!existingItem) {
+        // 새 제품을 장바구니에 추가
         state.items.push({
           id: newItem.id,
           price: newItem.price,
@@ -20,8 +26,9 @@ const cartSlice = createSlice({
           name: newItem.title
         });
       } else {
+        // 기존 제품의 수량과 총 가격을 업데이트
         existingItem.quantity++;
-        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+        existingItem.totalPrice += newItem.price;
       }
     },
     removeItemFromCart(state, action) {
@@ -29,8 +36,10 @@ const cartSlice = createSlice({
       const existingItem = state.items.find(item => item.id === id);
       state.totalQuantity--;
       if (existingItem.quantity === 1) {
+        // 제품 수량이 1개면, 제품을 장바구니에서 제거
         state.items = state.items.filter(item => item.id !== id);
       } else {
+        // 그렇지 않으면 수량을 감소
         existingItem.quantity--;
       }
     },
