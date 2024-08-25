ddec8cf13874afea12957826458f4f75f3b04b3b
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2024-08-25 22:14:27 +0900

    loader , custom error

--- 21 adv-project/frontend/src/pages/Events.js
+++ 21 adv-project/frontend/src/pages/Events.js
@@ -1,26 +1,27 @@
-import { Link } from "react-router-dom";
-const DUMMY_EVENTS = [
-    {
-        id:'e1',
-        title:'Some event',
-    },
-    {
-        id:'e2',
-        title:'Another event',
-    },
-];
+import { useLoaderData,json } from 'react-router-dom';
+import EventsList from '../components/EventsList';
 
-function EventsPage(){
-    return (
-      <>
-        <h1> EventsPage</h1>
-        <ul>
-          {DUMMY_EVENTS.map((event) => (
-            <li key={event.id}>
-              <Link to={event.id}>{event.title}</Link>
-            </li>
-          ))}
-        </ul>
-      </>
-    );}
+function EventsPage() {
+  const data = useLoaderData();
+  const events =data.events;
+
+  if(data.isError){
+    return <p>{data.message}</p>;
+  }
+  return (<EventsList events={events} />);
+}
 export default EventsPage;
+
+
+export async function loader(){
+        const response = await fetch("http://localhost:8080/events");
+
+        if (!response.ok) {
+            // return{ isError :true, message:'could not fetch events'}
+            // throw new Response(JSON.stringify({message:'could not fetch event'}));
+            return json({message:'could not fetch event'},{status:500,});
+        } else {
+          return response;
+        }
+    
+}
