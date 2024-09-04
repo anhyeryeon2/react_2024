474a8d662d5adce1126526bd91d87702ccf20b81
hyeryeon <100520490+anhyeryeon2@users.noreply.github.com>
2024-09-04 22:13:20 +0900

    add Form

--- 30 react-TS/src/App.tsx
+++ 30 react-TS/src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import './App.css';
 import Todos from './components/Todos';
+import Todo from './models/todo';
+import NewTodo from './components/NewTodo';
 function App() {
+  const todos =[
+    new Todo('리액트배우기'),
+    new Todo('타스배우기')
+  ];
+
   return (
     <div >
-     <Todos/>
+     <Todos items={todos}/>
+     <NewTodo/>
     </div>
   );
 }
